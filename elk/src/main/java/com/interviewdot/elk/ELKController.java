3bbad13e2fb313fd5dcba36730845d0d74bc5b36
SneakySri <114966288+SneakySri@users.noreply.github.com>
2022-11-08 11:52:23 +0530

    Add files via upload

---
+++ elk/src/main/java/com/interviewdot/elk/ELKController.java
@@ -0,0 +1,60 @@
+package com.interviewdot.elk;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.util.Date;
+
+@RestController
+public class ELKController {
+    private static final Logger LOG = LoggerFactory.getLogger(ELKController.class.getName());
+
+    @Autowired
+    RestTemplate restTemplete;
+
+    @Bean
+    RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+    @RequestMapping(value = "/elk")
+    public String helloWorld() {
+        String response = "Hello user ! " + new Date();
+        LOG.info("/elkdemo - &gt; " + response);
+
+        return response;
+    }
+    
+    @RequestMapping(value = "/exception")
+	public String exception() {
+		String response = "";
+		try {
+			throw new Exception("Opps Exception raised....");
+		} catch (Exception e) {
+			e.printStackTrace();
+//			LOG.error(e);
+
+			StringWriter sw = new StringWriter();
+			PrintWriter pw = new PrintWriter(sw);
+			e.printStackTrace(pw);
+			String stackTrace = sw.toString();
+			LOG.error("Exception - " + stackTrace);
+			response = stackTrace;
+		}
+
+		return response;
+	}
+	
+
+
+    
+}
